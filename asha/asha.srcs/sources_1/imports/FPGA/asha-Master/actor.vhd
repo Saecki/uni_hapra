ca67696a960e3a5da6e61d5bd21b01e2790ec4e8
Tobias Schmitz <tobiasschmitz2001@gmail.com>
2024-06-12 20:13:16 +0200

    refactor: actor

--- asha/asha.srcs/sources_1/imports/FPGA/asha-Master/actor.vhd
+++ asha/asha.srcs/sources_1/imports/FPGA/asha-Master/actor.vhd
@@ -44,34 +44,34 @@ architecture Behavioral of actor is
 
     -- Zustandsautomat für Modus Auswahl
     -- type of state machine(M for Modus).
-
     type state_typeM is (
-        Asha1, Asha2, Asha3,
-        SensorRead1, SensorRead2, SensorRead3,
-        ManualActor1, ManualActor2, ManualActor3,
-        AutoActor1, AutoActor2, AutoActor3,
-        Bluetooth1, Bluetooth2, Bluetooth3
+        Asha,
+        SensorRead,
+        ManualActor,
+        AutoActor,
+        Bluetooth
     );
 
     -- current and next state declaration.
-    signal current_m : state_typeM := Asha2;
-    signal next_m    : state_typeM := Asha2;
+    signal current_m : state_typeM := Asha;
+    signal next_m    : state_typeM := Asha;
+    signal button_m  : state_typeM := Asha;
 
     -- Zustandsautomat für Sensor Zustaende.
     -- type of state machine(S for Sensor).
-
     type state_typeS is (
-        Init, Init2,
-        Light, Light2,
-        TempIn, TempIn2,
-        TempOut, TempOut2,
-        Vibe, Vibe2,
-        Door, Door2
+        Init,
+        Light,
+        TempIn,
+        TempOut,
+        Vibe,
+        Door
     );
 
     -- current and next state declaration.
     signal current_s : state_typeS := Init;
     signal next_s    : state_typeS := Init;
+    signal button_s  : state_typeS := Init;
 
 begin
 
@@ -82,7 +82,7 @@ begin
         -- Beim Reset die current Zustände auf die initialen Zustände setzen
         if (Reset = '1') then
             current_s <= Init;
-            current_m <= Asha2;
+            current_m <= Asha;
         elsif rising_edge(Clock) then
             current_s <= next_s;
             current_m <= next_m;
@@ -96,142 +96,66 @@ begin
     FSM_modul : process (current_m, ButtonsIn(0), ButtonsIn(1)) is
     begin
 
+        if (ButtonsIn(0) = '0' and ButtonsIn(1) = '0') then
+            next_m <= button_m;
+        end if;
+
         case current_m is
 
-            -- Zustand 1 ist der Zustand der beim loslassen des Button 0 einen Modus zurück schaltet
-            when Asha1 =>
-
-                if (ButtonsIn(0) = '0') then
-                    next_m <= Bluetooth2;
-                else
-                    next_m <= Asha1;
-                end if;
-
             -- Asha 2 ist der Basiszustand des Modus
-            when Asha2 =>
+            when Asha =>
 
-                -- drückt man Button 0, beginnen wir den Vorgang des zurück Schaltens, indem wir in Zustand 1 wechseln
+                -- drückt man Button 0, beginnen wir den Vorgang des Zurückschaltens,
+                -- indem in `button_m` der Zustand vorgemerkt wird
                 if (ButtonsIn(0) = '1') then
-                    next_m <= Asha1;
-                -- drückt man Button 0, beginnen wir den Vorgang des vorwärts Schaltens, indem wir in Zustand 3 wechseln
+                    button_m <= Bluetooth;
+                -- drückt man Button 0, beginnen wir den Vorgang des Vorwärtsschaltens,
+                -- indem in `button_m` der Zustand vorgemerkt wird
                 elsif (ButtonsIn(1) = '1') then
-                    next_m <= Asha3;
+                    button_m <= SensorRead;
                 else
-                    next_m <= Asha2;
-                end if;
-
-            -- Asha3 ist der Zustand, der beim Loslassen des Buttons 1 einen Modus vorwärts schaltet
-            when Asha3 =>
-
-                if (ButtonsIn(1) = '0') then
-                    next_m <= SensorRead2;
-                else
-                    next_m <= Asha3;
+                    button_m <= Asha;
                 end if;
 
             -- Analog zum Init Zustand "Asha" sind die weiteren Zustände programmiert.
-            when SensorRead1 =>
-
-                if (ButtonsIn(0) = '0') then
-                    next_m <= Bluetooth2;
-                else
-                    next_m <= SensorRead1;
-                end if;
-
-            when SensorRead2 =>
+            when SensorRead =>
 
                 if (ButtonsIn(0) = '1') then
-                    next_m <= SensorRead1;
+                    button_m <= Asha;
                 elsif (ButtonsIn(1) = '1') then
-                    next_m <= SensorRead3;
+                    button_m <= ManualActor;
                 else
-                    next_m <= SensorRead2;
+                    button_m <= SensorRead;
                 end if;
 
-            when SensorRead3 =>
-
-                if (ButtonsIn(1) = '0') then
-                    next_m <= ManualActor2;
-                else
-                    next_m <= SensorRead3;
-                end if;
-
-            when ManualActor1 =>
-
-                if (ButtonsIn(0) = '0') then
-                    next_m <= SensorRead2;
-                else
-                    next_m <= ManualActor1;
-                end if;
-
-            when ManualActor2 =>
+            when ManualActor =>
 
                 if (ButtonsIn(0) = '1') then
-                    next_m <= ManualActor1;
+                    button_m <= SensorRead;
                 elsif (ButtonsIn(1) = '1') then
-                    next_m <= ManualActor3;
+                    button_m <= AutoActor;
                 else
-                    next_m <= ManualActor2;
+                    button_m <= ManualActor;
                 end if;
 
-            when ManualActor3 =>
-
-                if (ButtonsIn(1) = '0') then
-                    next_m <= AutoActor2;
-                else
-                    next_m <= ManualActor3;
-                end if;
-
-            when AutoActor1 =>
-
-                if (ButtonsIn(0) = '0') then
-                    next_m <= ManualActor2;
-                else
-                    next_m <= AutoActor1;
-                end if;
-
-            when AutoActor2 =>
+            when AutoActor =>
 
                 if (ButtonsIn(0) = '1') then
-                    next_m <= AutoActor1;
+                    button_m <= ManualActor;
                 elsif (ButtonsIn(1) = '1') then
-                    next_m <= AutoActor3;
+                    button_m <= Bluetooth;
                 else
-                    next_m <= AutoActor2;
+                    button_m <= AutoActor;
                 end if;
 
-            when AutoActor3 =>
-
-                if (ButtonsIn(1) = '0') then
-                    next_m <= Bluetooth2;
-                else
-                    next_m <= AutoActor3;
-                end if;
-
-            when Bluetooth1 =>
-
-                if (ButtonsIn(0) = '0') then
-                    next_m <= AutoActor2;
-                else
-                    next_m <= Bluetooth1;
-                end if;
-
-            when Bluetooth2 =>
+            when Bluetooth =>
 
                 if (ButtonsIn(0) = '1') then
-                    next_m <= Bluetooth1;
+                    button_m <= AutoActor;
                 elsif (ButtonsIn(1) = '1') then
-                    next_m <= Bluetooth3;
+                    button_m <= SensorRead;
                 else
-                    next_m <= Bluetooth2;
-                end if;
-
-            when Bluetooth3 =>
-
-                if (ButtonsIn(1) = '0') then
-                    next_m <= SensorRead2;
-                else
-                    next_m <= Bluetooth3;
+                    button_m <= Bluetooth;
                 end if;
 
         end case;
@@ -246,129 +170,82 @@ begin
     FSM_comb : process (current_s, current_m, ButtonsIn(2), ADCRegister, SensorVibe, SensorDoor) is
     begin
 
+        if (ButtonsIn(2) = '0') then
+            next_s <= button_s;
+        end if;
+
         -- Hier wird der Sensor Zustand abhängig von den Buttons gesetzt
         -- Modus 0: "ASHA" Auf 7 Segment Anzeige
         case current_m is
 
-            when Asha1|Asha2|Asha3 => -- ASHA state
+            when Asha => -- ASHA state
 
                 LEDsOut           <= b"111111";
                 SevenSegmentValue <= x"FFFF";
 
-            when SensorRead1|SensorRead2|SensorRead3 =>
+            when SensorRead =>
+
+                SevenSegmentValue(15 downto 12) <= x"A";
 
                 case current_s is
 
-                    -- init ist der Basiszustand. von hier kannn man button 2 drücken, um in Init2 zu wechseln. Lässt man den Button los wechselt man von Init2 in den nächsten Zustand. Analog dazu sind die weitern Zustände programmiert
+                    -- init ist der Basiszustand. von hier kannn man button 2 drücken, um den nächsten Zustand vorzumerken.
+                    -- Lässt man den Button los wechselt man in den nächsten Zustand. Analog dazu sind die weitern Zustände programmiert
                     when Init =>
 
-                        LEDsOut           <= b"100000";
-                        SevenSegmentValue <= x"FFFF";
+                        LEDsOut                        <= b"100000";
+                        SevenSegmentValue(11 downto 0) <= x"FFF";
 
                         if (ButtonsIn(2) = '1') then
-                            next_s <= Init2;
+                            button_s <= Light;
                         else
-                            next_s <= Init;
-                        end if;
-
-                    when Init2 =>
-
-                        LEDsOut <= b"000000";
-
-                        if (ButtonsIn(2) = '0') then
-                            next_s <= Light;
-                        else
-                            next_s <= Init2;
+                            button_s <= Init;
                         end if;
 
                     when Light =>
 
-                        LEDsOut                         <= b"010000";
-                        SevenSegmentValue(15 downto 12) <= x"A";
+                        LEDsOut <= b"010000";
                         -- Hier müssen die relevanten Sensordaten auf das SevenSegment geschrieben
                         SevenSegmentValue(11 downto 0) <= ADCRegister(3);
 
                         if (ButtonsIn(2) = '1') then
-                            next_s <= Light2;
+                            button_s <= TempIn;
                         else
-                            next_s <= Light;
-                        end if;
-
-                    when Light2 =>
-
-                        LEDsOut <= b"000000";
-
-                        if (ButtonsIn(2) = '0') then
-                            next_s <= TempIn;
-                        else
-                            next_s <= Light2;
+                            button_s <= Light;
                         end if;
 
                     when TempIn =>
 
-                        LEDsOut                         <= b"001000";
-                        SevenSegmentValue(15 downto 12) <= x"B";
-                        SevenSegmentValue(11 downto 0)  <= ADCRegister(0);
+                        LEDsOut                        <= b"001000";
+                        SevenSegmentValue(11 downto 0) <= ADCRegister(0);
 
                         if (ButtonsIn(2) = '1') then
-                            next_s <= TempIn2;
+                            button_s <= TempOut;
                         else
-                            next_s <= TempIn;
-                        end if;
-
-                    when TempIn2 =>
-
-                        LEDsOut <= b"000000";
-
-                        if (ButtonsIn(2) = '0') then
-                            next_s <= TempOut;
-                        else
-                            next_s <= TempIn2;
+                            button_s <= TempIn;
                         end if;
 
                     when TempOut =>
 
-                        LEDsOut                         <= b"000100";
-                        SevenSegmentValue(15 downto 12) <= x"C";
-                        SevenSegmentValue(11 downto 0)  <= ADCRegister(1);
+                        LEDsOut                        <= b"000100";
+                        SevenSegmentValue(11 downto 0) <= ADCRegister(1);
 
                         if (ButtonsIn(2) = '1') then
-                            next_s <= TempOut2;
+                            button_s <= Vibe;
                         else
-                            next_s <= TempOut;
-                        end if;
-
-                    when TempOut2 =>
-
-                        LEDsOut <= b"000000";
-
-                        if (ButtonsIn(2) = '0') then
-                            next_s <= Vibe;
-                        else
-                            next_s <= TempOut2;
+                            button_s <= TempOut;
                         end if;
 
                     when Vibe =>
 
-                        LEDsOut                         <= b"000010";
-                        SevenSegmentValue(15 downto 12) <= x"D";
-                        SevenSegmentValue(11 downto 1)  <= b"00000000000";
-                        SevenSegmentValue(0)            <= SensorVibe;
+                        LEDsOut                        <= b"000010";
+                        SevenSegmentValue(11 downto 1) <= b"00000000000";
+                        SevenSegmentValue(0)           <= SensorVibe;
 
                         if (ButtonsIn(2) = '1') then
-                            next_s <= Vibe2;
+                            button_s <= Door;
                         else
-                            next_s <= Vibe;
-                        end if;
-
-                    when Vibe2 =>
-
-                        LEDsOut <= b"000000";
-
-                        if (ButtonsIn(2) = '0') then
-                            next_s <= Door;
-                        else
-                            next_s <= Vibe2;
+                            button_s <= Vibe;
                         end if;
 
                     when Door =>
@@ -379,19 +256,9 @@ begin
                         SevenSegmentValue(0)            <= SensorDoor;
 
                         if (ButtonsIn(2) = '1') then
-                            next_s <= Door2;
+                            button_s <= Light;
                         else
-                            next_s <= Door;
-                        end if;
-
-                    when Door2 =>
-
-                        LEDsOut <= b"000000";
-
-                        if (ButtonsIn(2) = '0') then
-                            next_s <= Light;
-                        else
-                            next_s <= Door2;
+                            button_s <= Door;
                         end if;
 
                 end case;
@@ -400,48 +267,57 @@ begin
             -- Modus 2: Manuelle Aktorsteuerung
             -- nur erlauben, wenn keine Regelung aktiv ist!
             -- Ansteuerung der Actoren mittels Switches. Seven Segment Value wird je nach zustand geändert und dient ausschließlich als test
-            when ManualActor1|ManualActor2|ManualActor3 =>
+            when ManualActor =>
 
-                SevenSegmentValue(15 downto 0) <= b"1111111111111110";
+                SevenSegmentValue(15 downto 12) <= x"B";
 
                 if (Switches(0) = '1') then
-                    PWM1FanInsideValue             <= "11111111";
-                    SevenSegmentValue(15 downto 0) <= b"1111111111111111";
+                    PWM1FanInsideValue              <= x"FF";
+                    SevenSegmentValue(11 downto 10) <= x"1";
                 else
-                    PWM1FanInsideValue <= "00000000";
+                    PWM1FanInsideValue              <= x"00";
+                    SevenSegmentValue(11 downto 10) <= x"0";
                 end if;
 
                 if (Switches(1) = '1') then
-                    PWM2FanOutsideValue            <= "11111111";
-                    SevenSegmentValue(15 downto 0) <= b"1111111111111100";
+                    PWM2FanOutsideValue           <= x"FF";
+                    SevenSegmentValue(9 downto 8) <= x"1";
                 else
-                    PWM2FanOutsideValue <= "00000000";
+                    PWM2FanOutsideValue           <= x"00";
+                    SevenSegmentValue(9 downto 8) <= x"0";
                 end if;
 
                 if (Switches(2) = '1') then
-                    PWM3LightValue                 <= "11111111";
-                    SevenSegmentValue(15 downto 0) <= b"1111111111111000";
+                    PWM3LightValue                <= x"FF";
+                    SevenSegmentValue(7 downto 4) <= x"1";
                 else
-                    PWM3LightValue <= "00000000";
+                    PWM3LightValue                <= x"00";
+                    SevenSegmentValue(7 downto 4) <= x"0";
                 end if;
 
                 PeltierDirection <= '1';
 
                 if (Switches(3) = '1') then
-                    PWM4PeltierValue               <= "11111111";
-                    SevenSegmentValue(15 downto 0) <= b"1111111111110000";
+                    PWM4PeltierValue              <= x"FF";
+                    SevenSegmentValue(3 downto 0) <= x"1";
                 else
-                    PWM4PeltierValue <= "00000000";
+                    PWM4PeltierValue              <= x"00";
+                    SevenSegmentValue(3 downto 0) <= x"0";
                 end if;
 
             -- Versuch 9
-            -- Modus 3: geregelte Aktorsteuerung
-            -- when ... TODO
+            -- TODO: Modus 3, geregelte Aktorsteuerung
+            when ManualActor =>
+
+                LEDsOut           <= b"000000";
+                SevenSegmentValue <= x"C000";
 
             -- Versuch 10
-            -- Modus 4: Steuerung ueber Smartphone-App
-            -- when ... TODO
-            when others =>
+            -- TODO: Modus 4, Steuerung ueber Smartphone-App
+            when Bluetooth =>
+
+                LEDsOut           <= b"000000";
+                SevenSegmentValue <= x"D000";
 
         -- DEFAULT Werte setzen TODO
 
